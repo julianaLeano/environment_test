7b84577c750dbf1cea7dfb0a134dc4775593c853
julianaLeano <julexd125@gmail.com>
2022-09-18 19:46:37 -0500

    users and user_books added

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,15 @@
 class Book < ApplicationRecord
+   has_many :users, through: :user_books
+   has_many :user_books
    validates :title, :author, :price, :published_date, presence: true
+end
+
+class UserBook < ApplicationRecord
+   belongs_to :user
+   belongs_to :book
+end
+
+class User < ApplicationRecord
+   has_many :books, through: :user_books
+   has_many :user_books
 end
